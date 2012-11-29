c079cfa7cead8b8a700c770b4e763a22600d4df0
Tom Rees <zephod@gmail.com>
2012-11-29 12:44:19 +0000

    Added dircetion control

--- control.py
+++ control.py
@@ -3,17 +3,27 @@
 from lib.Adafruit_PWM_Servo_Driver import PWM
 from lib import xbox_read
 import time
-import re
-
-# ===========================================================================
-# Example Code
-# ===========================================================================
 
 # Initialise the PWM device using the default address
-# bmp = PWM(0x40, debug=True)
 pwm = PWM(0x40, debug=True)
 
-s = re.compile('[ :]')
+# Initialise the GPIO output channels
+GPIO.setmode(GPIO.BCM)
+FOWARD = 17
+BACKWARD = 21
+GPIO.setup(FORWARD, GPIO.OUT)
+GPIO.setup(BACKWARD, GPIO.OUT)
+direction = None
+
+# Setup direction of travel
+def setDirection(new_direction):
+    if direction:
+        GPIO.output(direction,False)
+    GPIO.output(new_direction,True)
+    direction = new_dircetion
+
+setDirection(FORWARD)
+
 
 servoMin = 150  # Min pulse length out of 4096
 servoMax = 600  # Max pulse length out of 4096
@@ -31,4 +41,4 @@ for event in xbox_read.event_stream(deadzone=12000):
     # Left trigger controls the steering
     if event.key=='X1':
         steer = int( servoMid + (servoW*event.value)/32768 )
-        pwm.setPWM(15, 0, steer2)
+        pwm.setPWM(15, 0, steer)
